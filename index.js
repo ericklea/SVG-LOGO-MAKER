da1605806ec18a336f3df85d2eacc0d92192d361
L E <erickson.leah.j@gmail.com>
2023-05-01 21:26:10 -0600

    updated prompt

--- index.js
+++ index.js
@@ -3,11 +3,16 @@
   const inquirer = require('inquirer');
   const fs = require('fs');
   const path = require('path');
-    const shapesJS = require('./lib/shapes.js');
+  const shapesJS = require('./lib/shapes.js');
 
     // Array of questions for user input
 const questions = [
    { 
+    type: 'input',
+    name: 'shape',
+    message: 'Please enter the shape you would like to use',
+    },
+    {
     type: 'input',
     name: 'character length',
     message: 'How many characters would you like your logo to contain?',
@@ -15,19 +20,14 @@ const questions = [
     {
     type: 'input',
     name: 'Character type',
-    message: 'Please enter the characters you would like to use in order',
+    message: 'Please enter the characters you would like to use, in order',
     },
     {
     type: 'input',
     name: 'color',
     message: 'Please enter the color you would like to use',
     },
-    {
-    type: 'input',
-    name: 'shape',
-    message: 'Please enter the shape you would like to use',
-    },
-
+   
 ];
 
 //Function to write SVG file
@@ -38,7 +38,7 @@ function writeSVGFile(fileName, data) {
 function init() {
     inquirer.prompt(questions).then((answers) => {
         console.log(answers);
-        writeReadMeFile('logo.svg', shapesJS({...answers}));
+        writeSVGFile('logo.svg', shapesJS({...answers}));
     });
 }
 
