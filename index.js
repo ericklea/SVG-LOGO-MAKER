af5dc85c1554ee1fb28765a709ef1fbd8130584a
L E <erickson.leah.j@gmail.com>
2023-05-01 18:20:33 -0600

    entered prompts

--- index.js
+++ index.js
@@ -5,6 +5,29 @@
   const path = require('path');
     const shapesJS = require('./lib/shapes.js');
 
+    // Array of questions for user input
+const questions = [
+   { 
+    type: 'input',
+    name: 'character length',
+    message: 'How many characters would you like your logo to contain?',
+    },
+    {
+    type: 'input',
+    name: 'Character type',
+    message: 'Please enter the characters you would like to use in order',
+    },
+    {
+    type: 'input',
+    name: 'color',
+    message: 'Please enter the color you would like to use',
+    },
+    {
+    type: 'input',
+    name: 'shape',
+    message: 'Please enter the shape you would like to use',
+    },
+
 
 
 
